86221e0964883fe6f418b46c5d1e00703094a183
Haru <lucnguyen2407@gmail.com>
2025-03-27 11:08:40 +0700

    update navbar and mainbody

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,26 @@
-import { Suspense } from 'react';
-import dynamic from 'next/dynamic';
+import MainBody from '@/components/body/mainBody';
+import Footer from '@/components/footer/footer';
+import NavBar from '@/components/header/navBar';
 import { Box, Container, Spinner } from '@chakra-ui/react';
-
-// Dynamic imports for better code splitting
-const NavBar = dynamic(() => import('@/components/header/navBar'), {
-  loading: () => <Box h="16" />,
-});
-
-const MainBody = dynamic(() => import('@/components/body/mainBody'), {
-  loading: () => <Spinner size="xl" />,
-});
-
-const Footer = dynamic(() => import('@/components/footer/footer'), {
-  loading: () => <Box h="16" />,
-});
+import { Suspense } from 'react';
 
 export default function Home() {
   return (
-    <Box as="main" minH="100vh" display="flex" flexDir="column">
-      <Suspense fallback={<Box h="16" />}>
+    <Box as="main" minH="100vh" display="flex" flexDir="column" gap={8}>
+      <Box position={'sticky'} top={0} zIndex={100} backdropFilter="blur(10px)">
         <NavBar />
-      </Suspense>
+      </Box>
 
-      <Container maxW="container.lg" flex="1">
-        <Suspense fallback={<Spinner size="xl" />}>
-          <MainBody />
-        </Suspense>
+      <Container
+        maxW="container.lg"
+        flex="1"
+        py={8}
+        px={{ base: 6, md: 12, lg: 50 }}
+      >
+        <MainBody />
       </Container>
 
-      <Suspense fallback={<Box h="16" />}>
-        <Footer />
-      </Suspense>
+      <Footer />
     </Box>
   );
 }
--- src/components/body/mainBody.tsx
+++ src/components/body/mainBody.tsx
@@ -8,6 +8,7 @@ import {
   Image,
   List,
   SimpleGrid,
+  VStack,
 } from '@chakra-ui/react';
 import Link from 'next/link';
 import { useColorModeValue } from '../ui/color-mode';
@@ -17,161 +18,186 @@ import { RiMailLine } from 'react-icons/ri';
 
 export default function MainBody() {
   return (
-    <Container>
-      <Box
-        borderRadius="lg"
-        mb={6}
-        p={3}
-        textAlign="center"
-        bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
-        css={{ backdropFilter: 'blur(10px)' }}
-      >
-        Hello, I&apos;m an indie app developer based in Japan!
-      </Box>
-
-      <Box display={{ md: 'flex' }}>
-        <Box flexGrow={1}>
-          <Heading as="h2">Takuya Matsuyama</Heading>
-          <p>Digital Craftsman ( Artist / Developer / Designer )</p>
-        </Box>
+    <Container
+      maxW="container.md"
+      px={{ base: 6, md: 12, lg: 16 }} // Increased padding
+    >
+      <VStack width="full" gap={8} alignItems="stretch">
         <Box
-          flexShrink={0}
-          mt={{ base: 4, md: 0 }}
-          ml={{ md: 6 }}
+          borderRadius="lg"
+          mb={6}
+          p={3}
           textAlign="center"
+          bg={useColorModeValue('whiteAlpha.500', 'whiteAlpha.200')}
+          css={{ backdropFilter: 'blur(10px)' }}
         >
+          Hello, I&apos;m an indie app developer based in Japan!
+        </Box>
+
+        {/* Profile Section */}
+        <Box display={{ md: 'flex' }}>
+          <Box flexGrow={1}>
+            <Heading as="h2">Nguyen Tien Luc</Heading>
+            <p> Developer </p>
+          </Box>
           <Box
-            borderColor="whiteAlpha.800"
-            borderWidth={2}
-            borderStyle="solid"
-            w="100px"
-            h="100px"
-            display="inline-block"
-            borderRadius="full"
-            overflow="hidden"
+            flexShrink={0}
+            mt={{ base: 4, md: 0 }}
+            ml={{ md: 6 }}
+            textAlign="center"
           >
-            <Image
-              src="https://bit.ly/naruto-sage"
-              boxSize="150px"
+            <Box
+              borderColor="whiteAlpha.800"
+              borderWidth={2}
+              borderStyle="solid"
+              w="100px"
+              h="100px"
+              display="inline-block"
               borderRadius="full"
-              fit="cover"
-              alt="Naruto Uzumaki"
-            />
+              overflow="hidden"
+            >
+              <Image
+                src="https://bit.ly/naruto-sage"
+                boxSize="150px"
+                borderRadius="full"
+                fit="cover"
+                alt="Naruto Uzumaki"
+              />
+            </Box>
           </Box>
         </Box>
-      </Box>
 
-      <Heading as="h3">Work</Heading>
-      <Paragraph>
-        Takuya is a freelance and a full-stack developer based in Osaka with a
-        passion for building digital services/stuff he wants. He has a knack for
-        all things launching products, from planning and designing all the way
-        to solving real-life problems with code. When not online, he loves
-        hanging out with his camera. Currently, he is living off of his own
-        product called{' '}
-        <Link href="/works/inkdrop" passHref scroll={false}>
-          Inkdrop
-        </Link>
-        . He publishes content for marketing his products and his YouTube
-        channel called &quot;
-        <Link href="https://www.youtube.com/devaslife" passHref target="_blank">
-          Dev as Life
-        </Link>
-        &quot; has more than 100k subscribers.
-      </Paragraph>
-      <Box my={4}>
-        <Button colorScheme="teal">My portfolio</Button>
-      </Box>
+        {/* Work Section */}
+        <VStack gap={4} alignItems="flex-start">
+          <Heading as="h3">Work</Heading>
+          <Paragraph>
+            Takuya is a freelance and a full-stack developer based in Osaka with
+            a passion for building digital services/stuff he wants. He has a
+            knack for all things launching products, from planning and designing
+            all the way to solving real-life problems with code. When not
+            online, he loves hanging out with his camera. Currently, he is
+            living off of his own product called{' '}
+            <Link href="/works/inkdrop" passHref scroll={false}>
+              Inkdrop
+            </Link>
+            . He publishes content for marketing his products and his YouTube
+            channel called &quot;
+            <Link
+              href="https://www.youtube.com/devaslife"
+              passHref
+              target="_blank"
+            >
+              Dev as Life
+            </Link>
+            &quot; has more than 100k subscribers.
+          </Paragraph>
+          <Box my={4}>
+            <Button colorScheme="teal">My portfolio</Button>
+          </Box>
+        </VStack>
 
-      <Heading as="h3">Bio</Heading>
-      <BioSection>
-        <BioYear>1984</BioYear>
-        Born in Osaka (大阪), Japan.
-      </BioSection>
-      <BioSection>
-        <BioYear>2010</BioYear>
-        Completed the Master&apos;s Program in the Graduate School of
-        Information Science at Nara Institute of Science and Technology
-        (奈良先端科学技術大学院大学情報科学研究科修士課程)
-      </BioSection>
-      <BioSection>
-        <BioYear>2010</BioYear>
-        Worked at Yahoo! Japan (ヤフー株式会社入社)
-      </BioSection>
-      <BioSection>
-        <BioYear>2012 to present</BioYear>
-        Working as a freelancer
-      </BioSection>
+        {/* Bio Section */}
+        <VStack gap={4} alignItems="flex-start">
+          <Heading as="h3">Bio</Heading>
+          <BioSection>
+            <BioYear>1984</BioYear>
+            Born in Osaka (大阪), Japan.
+          </BioSection>
+          <BioSection>
+            <BioYear>2010</BioYear>
+            Completed the Master&apos;s Program in the Graduate School of
+            Information Science at Nara Institute of Science and Technology
+            (奈良先端科学技術大学院大学情報科学研究科修士課程)
+          </BioSection>
+          <BioSection>
+            <BioYear>2010</BioYear>
+            Worked at Yahoo! Japan (ヤフー株式会社入社)
+          </BioSection>
+          <BioSection>
+            <BioYear>2012 to present</BioYear>
+            Working as a freelancer
+          </BioSection>
+        </VStack>
 
-      <Heading as="h3">I ♥</Heading>
-      <Paragraph>
-        Art, Music,{' '}
-        <Link href="https://illust.odoruinu.net/" target="_blank">
-          Drawing
-        </Link>
-        , Playing Drums,{' '}
-        <Link href="https://500px.com/p/craftzdog" target="_blank">
-          Photography
-        </Link>
-        , Leica, Machine Learning
-      </Paragraph>
+        {/* I Love Section */}
+        <VStack gap={4} alignItems="flex-start">
+          <Heading as="h3">I ♥</Heading>
+          <Paragraph>
+            Art, Music,{' '}
+            <Link href="https://illust.odoruinu.net/" target="_blank">
+              Drawing
+            </Link>
+            , Playing Drums,{' '}
+            <Link href="https://500px.com/p/craftzdog" target="_blank">
+              Photography
+            </Link>
+            , Leica, Machine Learning
+          </Paragraph>
+        </VStack>
 
-      <Heading as="h3">On the web</Heading>
-      <List.Root>
-        <List.Item>
-          <Link href="https://github.com/craftzdog" target="_blank">
-            <Button variant="ghost" colorScheme="teal">
-              @craftzdog
-            </Button>
-          </Link>
-        </List.Item>
-        <List.Item>
-          <Link href="https://twitter.com/inkdrop_app" target="_blank">
-            <Button variant="ghost" colorScheme="teal">
-              @inkdrop_app (English)
-            </Button>
-          </Link>
-        </List.Item>
-        <List.Item>
-          <Link href="https://twitter.com/craftzdog" target="_blank">
-            <Button variant="ghost" colorScheme="teal">
-              @craftzdog (日本語)
-            </Button>
-          </Link>
-        </List.Item>
-        <List.Item>
-          <Link href="https://instagram.com/craftzdog" target="_blank">
-            <Button variant="ghost" colorScheme="teal">
-              @craftzdog
-            </Button>
-          </Link>
-        </List.Item>
-      </List.Root>
+        {/* Social Section */}
+        <VStack gap={4} alignItems="flex-start">
+          <Heading as="h3">On the web</Heading>
+          <List.Root>
+            <List.Item>
+              <Link href="https://github.com/craftzdog" target="_blank">
+                <Button variant="ghost" colorScheme="teal">
+                  @craftzdog
+                </Button>
+              </Link>
+            </List.Item>
+            <List.Item>
+              <Link href="https://twitter.com/inkdrop_app" target="_blank">
+                <Button variant="ghost" colorScheme="teal">
+                  @inkdrop_app (English)
+                </Button>
+              </Link>
+            </List.Item>
+            <List.Item>
+              <Link href="https://twitter.com/craftzdog" target="_blank">
+                <Button variant="ghost" colorScheme="teal">
+                  @craftzdog (日本語)
+                </Button>
+              </Link>
+            </List.Item>
+            <List.Item>
+              <Link href="https://instagram.com/craftzdog" target="_blank">
+                <Button variant="ghost" colorScheme="teal">
+                  @craftzdog
+                </Button>
+              </Link>
+            </List.Item>
+          </List.Root>
 
-      <SimpleGrid columns={[1, 2, 2]} gap={6}>
-        <Link href="https://www.youtube.com/devaslife" target="_blank">
-          <GridItem title="Dev as Life">
-            My YouTube channel (&gt;200k subs)
-          </GridItem>
-        </Link>
-        <Link href="https://www.inkdrop.app/" target="_blank">
-          <GridItem title="Inkdrop">A Markdown note-taking app</GridItem>
-        </Link>
-      </SimpleGrid>
+          <SimpleGrid columns={[1, 2, 2]} gap={6}>
+            <Link href="https://www.youtube.com/devaslife" target="_blank">
+              <GridItem title="Dev as Life">
+                My YouTube channel (&gt;200k subs)
+              </GridItem>
+            </Link>
+            <Link href="https://www.inkdrop.app/" target="_blank">
+              <GridItem title="Inkdrop">A Markdown note-taking app</GridItem>
+            </Link>
+          </SimpleGrid>
+        </VStack>
 
-      <Heading as="h3">Newsletter</Heading>
-      <p>
-        Join me on a behind-the-scenes coding journey. Weekly updates on
-        projects, tutorials, and videos
-      </p>
+        {/* Newsletter Section */}
+        <VStack gap={4} alignItems="flex-start">
+          <Heading as="h3">Newsletter</Heading>
+          <p>
+            Join me on a behind-the-scenes coding journey. Weekly updates on
+            projects, tutorials, and videos
+          </p>
 
-      <Box my={4}>
-        <Link href="https://www.devas.life/" passHref scroll={false}>
-          <Button>
-            <RiMailLine /> EmailSign up my newsletter here
-          </Button>
-        </Link>
-      </Box>
+          <Box my={4}>
+            <Link href="https://www.devas.life/" passHref scroll={false}>
+              <Button>
+                <RiMailLine /> EmailSign up my newsletter here
+              </Button>
+            </Link>
+          </Box>
+        </VStack>
+      </VStack>
     </Container>
   );
 }
